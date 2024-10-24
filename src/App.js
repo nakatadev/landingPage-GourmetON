af1257ab794ced117802c5c3a37590efa0d7104a
Rodrigo <nakataecomm@gmail.com>
2024-10-24 20:12:38 -0300

    ultimas alterações

--- src/App.js
+++ src/App.js
@@ -1,31 +1,3 @@
-
-import React from 'react';
-import Header from './componentes/header/header.js';
-import Apresentacao from './componentes/apresentacao/apresentacao.js';
-import Conheca from './componentes/conheca/conheca.js';
-import Porque from './componentes/porque/porque.js';
-import Funcionalidades from './componentes/funcionalidades/funcionalidades.js';
-import MaisVendidos from './componentes/maisvendidos/maisvendidos.js';
-import Formulario from './componentes/formulario/formulario.js';
-import Footer from './componentes/footer/footer.js';
-import Depoimentos from './componentes/depoimentos/depoimentos.js'
-
-function App() {
-    return (
-        <div className='App'>
-            <Header />
-            <Apresentacao />
-            <Conheca />
-            <Porque />
-            <Funcionalidades />
-            <MaisVendidos />
-            <Depoimentos />
-            <Formulario />
-            <Footer />
-        </div>
-    )
-};
-
 import React from 'react';
 import Header from './componentes/header/header.js';
 import Apresentacao from './componentes/apresentacao/apresentacao.js';
